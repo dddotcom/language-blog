ae1efd03e4a7bb160125f27ccd311e9d492d1dfc
dddotcom <dddotcom@gmail.com>
2020-06-01 19:39:22 -0700

    move words into json files

--- src/contexts/TagalogContext.js
+++ src/contexts/TagalogContext.js
@@ -1,41 +1,50 @@
 import React, { createContext, useState, useEffect } from 'react';
+import * as adjectivesJson from '../salita/adjectives.json';
+import * as verbsJson from '../salita/verbs.json';
+import * as grammarJson from '../salita/grammar.json';
+import * as nounsJson from '../salita/nouns.json';
 
 export const TagalogContext = createContext();
 
 export const TagalogContextProvider = (props) => {
     const [adjectives, setAdjectives] = useState([]);
     const [verbs, setVerbs] = useState([]);
-    // const [nouns, setNouns] = useState([]);
-    // const [grammar, setGrammar] = useState([]);
+    const [nouns, setNouns] = useState([]);
+    const [grammar, setGrammar] = useState([]);
 
     const stall = async(stallTime = 2000) => {
         await new Promise(resolve => setTimeout(resolve, stallTime));
     }
     const getAdjectives = async () => {
         await stall();
-        let adj = await Promise.resolve([
-            { type: 'adjective', tagalog: 'basa', english: 'wet', 'tagalogSentence': 'Basang aso. ', 'englishSentence': 'Wet dog.'},
-            { type: 'adjective', tagalog: 'bago', english: 'new', 'tagalogSentence': 'Bagong damit.', 'englishSentence': 'New clothing.'},
-            { type: 'adjective', tagalog: 'bukas', english: 'open', 'tagalogSentence': 'Bukas na tindihan ', 'englishSentence': 'Open store'},
-            { type: 'adjective', tagalog: 'galit', english: 'angry', 'tagalogSentence': 'Galit na nanay. ', 'englishSentence': 'Angry mom.'},
-            { type: 'adjective', tagalog: 'gutom', english: 'hungry', 'tagalogSentence': 'Gutom na mga bata. ', 'englishSentence': 'Hungry children.'},
-            { type: 'adjective', tagalog: 'lasing', english: 'drunk', 'tagalogSentence': 'Lasing na tito. ', 'englishSentence': 'Drunk tito.'},
-        ])
+        let adj = adjectivesJson.default;
         setAdjectives(adj);
     }
     
     const getVerbs = async () => {
         await stall();
-        let verbs = await Promise.resolve([
-            { type: 'um-verb', tagalog: 'dating', english: 'arrive', 'tagalogSentence': 'Dumating siya sa bahay', 'englishSentence': 'He/she arrived at the house'},
-        ])
-        setVerbs(verbs);
+        let vbs = verbsJson.default;
+        setVerbs(vbs);
+    }
+
+    const getNouns = async () => {
+        await stall();
+        let ns = nounsJson.default;
+        setNouns(ns);
+    }
+    
+    const getGrammar = async () => {
+        await stall();
+        let gmr = grammarJson.default;
+        setGrammar(gmr);
     }
 
     useEffect(() => {
         const asyncFn = async() => {
             await getVerbs();
             await getAdjectives();
+            await getGrammar();
+            await getNouns();
         }
         
         asyncFn();
